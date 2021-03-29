c1f91e1a7c6c5cea62c19ba2500eb620a9deff59
ricpie <ricpie@gmail.com>
2021-03-29 13:00:23 -0600

    updated import

--- r_scripts/import_odk.R
+++ r_scripts/import_odk.R
@@ -5,52 +5,47 @@
 library(data.table)
 library(readr)
 
-start_path = "~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_InstrumentStart.csv/MNCH_InstrumentStart.csv"
-start_path_b = "~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_InstrumentStart.csv/MNCH_InstrumentStart-D_householdmembersrepeat.csv"
-
-odkstart_b = read_csv(start_path_b,
+#Import ODK start files
+odkstart_b = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_InstrumentStart.csv/MNCH_InstrumentStart-D_householdmembersrepeat.csv",
                       col_names = TRUE,
                       col_types = cols(.default = col_character())) %>% 
   dplyr::select(-KEY) %>%
   pivot_wider(names_from = c(1), values_from = c(3:ncol(.)-1)) %>% 
   rename(KEY = PARENT_KEY)
 
-odkstart = read_csv(start_path,
+odkstart = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_InstrumentStart.csv/MNCH_InstrumentStart.csv",
                     col_names = TRUE,
                     col_types = cols(.default = col_character())) %>% 
   left_join(odkstart_b,by = "KEY")
 
 
 
-end_path = "~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_Instrumentend.csv/MNCH_Instrumentend.csv"
-
-odkend = read_csv(end_path,
-                  col_names = TRUE,
-                  col_types = cols(.default = col_character())) 
-
-end_paths = list.files("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_Instrumentend.csv/",full.names = TRUE) %>% 
-  grep("MNCH_InstrumentEnd-", ., ignore.case = TRUE, value = TRUE)
-
-end_variable = as.data.frame(
+#Import ODK end files
+odkend_multiple = as.data.frame(
   rbindlist(
-    lapply(
-      end_paths,
+    #Import a bunch of variables saved separately.
+    lapply( 
+      list.files("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_Instrumentend.csv/",full.names = TRUE) %>% 
+        grep("MNCH_InstrumentEnd-", ., ignore.case = TRUE, value = TRUE),
       function(x) read_csv(x) %>% 
         dplyr::select(-KEY) %>%
         gather(key,value,-PARENT_KEY,-c(1))),
     fill=TRUE))  %>% 
-  unite("idx",c(E1.1_pos_index,F1.1_pos_index,G1.1_pos_index,H1.1_pos_index,I1.1_pos_index,K_pos_index,L1.1_pos_index,M1.1_pos_index),
+  unite("idx",c(E1.1_pos_index,F1.1_pos_index,G1.1_pos_index,H1.1_pos_index,I1.1_pos_index,
+                K_pos_index,L1.1_pos_index,M1.1_pos_index),
         sep = ",", remove = FALSE,na.rm = TRUE) %>% 
-  dplyr::select(idx,KEY,key,value) %>% 
-  spread(key,value)
-
-odkend_b = read_csv(end_path_b,
-                    col_names = TRUE,
-                    col_types = cols(.default = col_character())) %>% 
-  dplyr::select(-PARENT_KEY) %>%
-  pivot_wider(names_from = hh_pos_index, values_from = c(D_membersex,D_memberageyears,D_memberagemonths))
+  dplyr::select(idx,PARENT_KEY,key,value) %>% 
+  # spread(key,value) %>% 
+  pivot_wider(names_from = c(idx,key), values_from = value) %>% 
+  rename(KEY = PARENT_KEY)  
 
 
 
+odkend = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_Instrumentend.csv/MNCH_Instrumentend.csv",
+                  col_names = TRUE,
+                  col_types = cols(.default = col_character())) %>% 
+  left_join(odkend_multiple,by = "KEY")
 
-saveRDS(meta_merged,'processed data/metadata_merged.RDS')
+
+saveRDS(odkstart,'~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/odkstart.RDS')
+saveRDS(odkend,'~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/odkend.RDS')
