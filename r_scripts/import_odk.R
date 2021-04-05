26985e9a745cac4e68a378668ac941dffabf7035
ricpie <ricpie@gmail.com>
2021-04-05 17:03:27 -0600

    updated odk import

--- r_scripts/import_odk.R
+++ r_scripts/import_odk.R
@@ -3,6 +3,7 @@
 #R. Piedrahita 07-01-2020
 #Code to ingest ODK data 
 library(data.table)
+library(tidyverse)
 library(readr)
 
 #Import ODK start files
@@ -16,8 +17,12 @@ odkstart_b = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH
 odkstart = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_InstrumentStart.csv/MNCH_InstrumentStart.csv",
                     col_names = TRUE,
                     col_types = cols(.default = col_character())) %>% 
-  left_join(odkstart_b,by = "KEY")
-
+  left_join(odkstart_b,by = "KEY") %>% 
+  dplyr::mutate(datetime_start = as.POSIXct(paste0(`A_samplingtype-A2_date`,`A_samplingtype-A2.1_time`)),
+                hhid = `consentgiven-C_HHInfo-C1_hhid`) %>% 
+  dplyr::select(-rcodef_ref,-rcodef_othersp,-`meta-instanceID`,-SubmitterID,-SubmitterName,-AttachmentsPresent,
+                -AttachmentsExpected,-Status,-`consentgiven-C_HHInfo-C1_hhid`) %>% 
+  setNames(gsub("[[:punct:]]","", names(.))) 
 
 
 #Import ODK end files
@@ -44,8 +49,17 @@ odkend_multiple = as.data.frame(
 odkend = read_csv("~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/MNCH_Instrumentend.csv/MNCH_Instrumentend.csv",
                   col_names = TRUE,
                   col_types = cols(.default = col_character())) %>% 
-  left_join(odkend_multiple,by = "KEY")
+  left_join(odkend_multiple,by = "KEY") %>% 
+  dplyr::mutate(datetime_end = as.POSIXct(paste0(`A_samplingtype-A2_date`,`G_End-G1_endtime`))+86400,
+                hhid = `C_SampleEnd-C0_hhid`) %>% 
+  dplyr::select(-`A_samplingtype-A2_date`,-`A_samplingtype-A3_time`,-`G_End-G1_endtime`,-SubmissionDate,
+                -AttachmentsPresent,-AttachmentsExpected,-Status,-`C_SampleEnd-err_hhid`,-KEY,-rcodef,
+                -`C_SampleEnd-C0_hhid`,-`C_SampleEnd-C0_hhid_conf`,-A0_fieldId,-`A_samplingtype-A1_instruments`) %>% 
+  setNames(gsub("[[:punct:]]","", names(.))) 
 
 
+odkstartend <- left_join(odkstart,odkend,by = "hhid")
 saveRDS(odkstart,'~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/odkstart.RDS')
 saveRDS(odkend,'~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/odkend.RDS')
+saveRDS(odkstartend,'~/Dropbox/Jacaranda Kenya Field Folder/Data/ODK Data/odkend.RDS')
+
